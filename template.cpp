a5fbe5b06b34d79eb989ff1584778d1176ea35be
wingkwong <wingkwong.code@gmail.com>
2020-05-16 11:51:56 +0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,21 @@
+/*
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std; 
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+typedef long long ll; 
+
+int main()  
+{ 
+    FAST_INP;
+    int n,t;
+    cin >> t;
+    while(t--){
+        cin >> n;
+       
+    }
+    return 0;
+} 
