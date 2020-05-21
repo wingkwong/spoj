2597ae94fe30a2471a675c6b72a60bd7659e5b97
wingkwong <wingkwong.code@gmail.com>
2020-05-21 14:51:36 +0800

    Create SMPWOW.cpp

---
+++ basic/SMPWOW.cpp
@@ -0,0 +1,20 @@
+/*
+SMPWOW - Wow
+https://www.spoj.com/problems/SMPWOW/
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    string s;
+    cin >> t;
+    cout << "W";
+    // print character 'o' t times
+    for(int i=0;i<t;i++){
+        cout << "o";
+    }
+    cout << "w";
+	return 0;
+}
